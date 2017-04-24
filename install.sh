76e52790359d5d78114ff26ca7aeca5a2bd5b4c1
d1rk <d1rk@gmx.de>
2017-04-24 18:07:50 +0200

    add database setup
    - php-mysql
    - create database / user
    - configure php.ini settings according to shopware documentation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ DOMAIN='example.com'
 
 apt-get update
 apt-get upgrade
-apt-get install -y software-properties-common htop letsencrypt
+apt-get install -y software-properties-common htop pwgen letsencrypt
 
 # mariadb
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
@@ -10,8 +10,15 @@ echo "deb http://mariadb.mirror.iweb.com/repo/10.0/ubuntu `lsb_release -cs` main
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server
 
+mysql -u root -p #empty password
+CREATE DATABASE IF NOT EXISTS shopware;
+CREATE USER 'shopware'@'localhost' IDENTIFIED BY 'athooghoJ6Eoj5EKocuB';
+GRANT ALL PRIVILEGES ON shopware.* TO 'shopware'@'localhost';
+FLUSH PRIVILEGES;
+
+
 # php
-apt-get install -y php7.0 php7.0-gd php7.0-zip php7.0-xml php7.0-intl php7.0-mbstring php7.0-curl
+apt-get install -y php7.0 php7.0-mysql php7.0-gd php7.0-zip php7.0-xml php7.0-intl php7.0-mbstring php7.0-curl
 
 # ioncube loader
 curl -#L http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz -o /tmp/ioncube.tar.gz
@@ -20,7 +27,11 @@ install -m 0644 /tmp/ioncube/ioncube_loader_lin_$(php -r 'printf("%s.%s", PHP_MA
 rm -rf /tmp/ioncube /tmp/ioncube.tar.gz
 echo "zend_extension=ioncube_loader.so" > /etc/php/7.0/mods-available/ioncube_loader.ini
 ln -s /etc/php/7.0/mods-available/ioncube_loader.ini /etc/php/7.0/cli/conf.d/00-ioncube_loader.ini && ln -s /etc/php/7.0/mods-available/ioncube_loader.ini /etc/php/7.0/fpm/conf.d/00-ioncube_loader.ini
+echo "memory_limit=512M" > /etc/php/7.0/mods-available/custom.ini
+echo "upload_max_filesize=24M" >> /etc/php/7.0/mods-available/custom.ini
+ln -s /etc/php/7.0/mods-available/custom.ini /etc/php/7.0/cli/conf.d/90-custom.ini && ln -s /etc/php/7.0/mods-available/custom.ini /etc/php/7.0/fpm/conf.d/90-custom.ini
 # check: php -r "echo "ionCube loader is installed: "var_export(extension_loaded('ionCube Loader'), true);"
+service php7.0-fpm reload
 
 # nginx
 apt-get install -y nginx
